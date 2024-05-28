9be13c6ccbd21a91a565b6543be355378518635f
yuemoj <yj_745@163.com>
2024-05-28 15:45:04 +0800

    # cache

--- YuemojKit/YuemojKit.h
+++ YuemojKit/YuemojKit.h
@@ -7,48 +7,48 @@
 
 #import <Foundation/Foundation.h>
 
-#if __has_include(<YuemojKit/YuemojKit.h>)
+//#if __has_include(<YuemojKit/YuemojKit.h>)
 //! Project version number for YuemojKit.
 FOUNDATION_EXPORT double YuemojKitVersionNumber;
 
 //! Project version string for YuemojKit.
 FOUNDATION_EXPORT const unsigned char YuemojKitVersionString[];
 
-#import <YuemojKit/Yuemoj.h>
-#import <YuemojKit/YuemojCoreTypes.h>
-#import <YuemojKit/YuemojMacros.h>
+//#import <YuemojKit/Yuemoj.h>
+//#import <YuemojKit/YuemojCoreTypes.h>
+//#import <YuemojKit/YuemojMacros.h>
 //#import <YuemojKit/Foundation+Yuemoj.h>
 //#import <YuemojKit/YuemojFoundationAbilities.h>
-#import <YuemojKit/UIKit+Yuemoj.h>
-#import <YuemojKit/YuemojUIAbilities.h>
-#import <YuemojKit/YJDataFillDataSource.h>
-#import <YuemojKit/YJDataFillerProtocol.h>
-#import <YuemojKit/YJDataFillTypes.h>
-#import <YuemojKit/YJEventBuilderProtocol.h>
-#import <YuemojKit/YJLayouterProtocol.h>
-#import <YuemojKit/YJLayoutModels.h>
-#import <YuemojKit/YJDataSource.h>
-#import <YuemojKit/YJGroupModel.h>
-#import <YuemojKit/YJOrderJSON.h>
-#import <YuemojKit/YJBoundedBuffer.h>
-//#import <YuemojKit/>
-#else
-#import "Yuemoj.h"
-#import "YuemojCoreTypes.h"
-#import "YuemojMacros.h"
+//#import <YuemojKit/UIKit+Yuemoj.h>
+//#import <YuemojKit/YuemojUIAbilities.h>
+//#import <YuemojKit/YJDataFillDataSource.h>
+//#import <YuemojKit/YJDataFillerProtocol.h>
+//#import <YuemojKit/YJDataFillTypes.h>
+//#import <YuemojKit/YJEventBuilderProtocol.h>
+//#import <YuemojKit/YJLayouterProtocol.h>
+//#import <YuemojKit/YJLayoutModels.h>
+//#import <YuemojKit/YJDataSource.h>
+//#import <YuemojKit/YJGroupModel.h>
+//#import <YuemojKit/YJOrderJSON.h>
+//#import <YuemojKit/YJBoundedBuffer.h>
+////#import <YuemojKit/>
+//#else
+//#import "Yuemoj.h"
+//#import "YuemojCoreTypes.h"
+//#import "YuemojMacros.h"
 //#import "Foundation+Yuemoj.h"
 //#import "YuemojFoundationAbilities.h"
-#import "UIKit+Yuemoj.h"
-#import "YuemojUIAbilities.h"
-#import "YJDataFillDataSource.h"
-#import "YJDataFillerProtocol.h"
-#import "YJDataFillTypes.h"
-#import "YJEventBuilderProtocol.h"
-#import "YJLayouterProtocol.h"
-#import "YJLayoutModels.h"
-#import "YJDataSource.h"
-#import "YJGroupModel.h"
-#import "YJOrderJSON.h"
-#import "YJBoundedBuffer.h"
+//#import "UIKit+Yuemoj.h"
+//#import "YuemojUIAbilities.h"
+//#import "YJDataFillDataSource.h"
+//#import "YJDataFillerProtocol.h"
+//#import "YJDataFillTypes.h"
+//#import "YJEventBuilderProtocol.h"
+//#import "YJLayouterProtocol.h"
+//#import "YJLayoutModels.h"
+//#import "YJDataSource.h"
+//#import "YJGroupModel.h"
+//#import "YJOrderJSON.h"
+//#import "YJBoundedBuffer.h"
 
-#endif
+//#endif
