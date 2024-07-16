dd14f88ebb958e24aa5a0dae04e4ec475f86f3f6
yuemoj <yj_745@163.com>
2024-07-16 15:51:18 +0800

    # style - ActionSheet布局问题修正.

--- YuemojKit/ComponentGallery/ActionController/YJActionSheetLayoutViewModel.m
+++ YuemojKit/ComponentGallery/ActionController/YJActionSheetLayoutViewModel.m
@@ -50,7 +50,9 @@
         YJActionSheetScene scene = YJActionSheetSceneFirstBtn + i;
         [btnItem bindView:provider(scene) withType:[self componentTypeForScene:scene] scene:scene];
         YJLayoutItemConstraintDescription *btnDescription = [btnItem makeItemDescription:^(id<YJLayoutConstraintAttributeDelegate>  _Nonnull maker) {
-            maker.top.equalTo(lastItem ? lastItem.bottom : @(self.actionTopMargin));
+            if (lastItem) maker.top.equalTo(lastItem.bottom);
+            else maker.top.equalTo(dragItem.bottom).yj_offset(self.actionTopMargin);
+//            maker.top.equalTo(lastItem ? lastItem.bottom : @(self.actionTopMargin));
             maker.leading.yj_offset(self.padding.left);
             maker.trailing.yj_offset(-self.padding.right);
             maker.height.yj_offset(self.actionHeight);
